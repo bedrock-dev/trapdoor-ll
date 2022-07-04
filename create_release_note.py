e794193e8088553b8b006464cd494201b5f47c24
hhhxiao <1417599491@qq.com>
2022-07-04 23:34:23 +0800

    Add CI/CD

---
+++ create_release_note.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    total = ''
+    with open('changelog.md', encoding='utf-8') as f:
+        total = f.readlines()
+    content = ''
+    index = 0
+    for x in total:
+        x = x.strip()
+        if len(x) != 0:
+            tokens = x.split(' ')
+            if tokens[0] == '###':
+                index += 1
+            if index == 2:
+                break
+            content += x + '\n'
+    print(content)
