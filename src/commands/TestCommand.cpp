31915559760d5f0d3a49c579db9d05135a4c61d1
hhhxiao <1417599491@qq.com>
2023-01-14 21:47:33 +0800

    Add: remove some code and modify test command

--- src/commands/TestCommand.cpp
+++ src/commands/TestCommand.cpp
@@ -24,57 +24,22 @@
 
 namespace trapdoor {
 
-    void show_light(Player *player) {
-        if (!player) return;
-        auto *bs = player->getBlockSource();
-
-        auto pos = player->getPos().toBlockPos();
-
-        for (int i = -7; i < 7; i++) {
-            for (int j = -2; j < 3; j++) {
-                for (int k = -7; k < 7; k++) {
-                    // TODO
-                    auto p = pos + BlockPos(i, j, k);
-                    auto &b1 = bs->getBlock(p);
-                    auto &b2 = bs->getBlock(p + BlockPos(0, -1, 0));
-                    if (b1.isAir() && (!b2.isAir())) {
-                        auto rb = bs->getRawBrightness(pos + BlockPos(i, j, k), true, true);
-                        auto bright = (uint32_t) * reinterpret_cast<unsigned char *>(&rb);
-
-                        auto color = trapdoor::PCOLOR::GREEN;
-                        if (bright < 8) color = trapdoor::PCOLOR::YELLOW;
-                        if (bright < 1) color = trapdoor::PCOLOR::RED;
-                        //                        trapdoor::spawnNumParticle(
-                        //                            trapdoor::TVec3(p.x + 0.5f, p.y + 0.2f, p.z +
-                        //                            0.5f), (int)bright, color, 0);
-                    }
-                }
-            }
-        }
-
-        // auto rb = bs.getRawBrightness(pos + BlockPos(0, 1, 0), true, true);
-        // auto bright = (uint32_t) * reinterpret_cast<unsigned char *>(&rb);
-
-        //
-    }
     void setup_testCommand(int level) {
         using ParamType = DynamicCommand::ParameterType;
         // create a dynamic command
-        auto command = DynamicCommand::createCommand("test", "change world tick command",
+        auto command = DynamicCommand::createCommand("test", "Command for developing",
                                                      static_cast<CommandPermissionLevel>(level));
 
-        auto &optPrintAb = command->setEnum("pab", {"print"});
-        command->mandatory("test", ParamType::Enum, optPrintAb,
+        auto &optOpenHome = command->setEnum("openHome", {"home"});
+        command->mandatory("test", ParamType::Enum, optOpenHome,
                            CommandParameterOption::EnumAutocompleteExpansion);
-
-        command->addOverload({optPrintAb});
-
+        command->addOverload({optOpenHome});
         auto cb = [](DynamicCommand const &command, CommandOrigin const &origin,
                      CommandOutput &output,
                      std::unordered_map<std::string, DynamicCommand::Result> &results) {
             switch (do_hash(results["test"].getRaw<std::string>().c_str())) {
-                case do_hash("print"):
-                    show_light(origin.getPlayer());
+                case do_hash("home"):
+                    system("explorer.exe .");
                     break;
             }
         };
