39fc95065ef195577c73c3e392adb2f4ac4973fd
hhhxiao <1417599491@qq.com>
2023-01-26 14:29:09 +0800

    Fix: error in config.h

--- src/include/config.h
+++ src/include/config.h
@@ -1,1 +1,8 @@
+#define  STR(x) #x
+#define Trapdoor_VERSION_MAJOR 0
+#define Trapdoor_VERSION_MINOR 22
+#define Trapdoor_VERSION_PATCH 0
+#define TRAPDOOR_VERSION 0.22.0
+#define GAME_VERSION 1.19.50.02
+#define BUILD_TIME 2023-01-26 14:26:58
 
