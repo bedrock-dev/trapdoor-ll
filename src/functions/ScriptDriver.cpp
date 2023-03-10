2bd9b7f4adcb6ebd3ea440978f346d244e117dad
hhhxiao <1417599491@qq.com>
2023-03-10 16:32:21 +0800

    Add: some apis

--- src/functions/ScriptDriver.cpp
+++ src/functions/ScriptDriver.cpp
@@ -3,7 +3,11 @@
 //
 
 #include "ScriptDriver.h"
-
+#include <mc/Block.hpp>
+#include <mc/BlockSource.hpp>
+#include <mc/Brightness.hpp>
+#include <mc/Dimension.hpp>
+#include <mc/Level.hpp>
 #include "TrapdoorMod.h"
 
 namespace trapdoor {
@@ -12,7 +16,6 @@ namespace trapdoor {
         this->errorStop = err;
         this->interval = inter;
         this->counter = 0;
-        this->engine.stack_clear();
         auto res = this->engine.script_file(fileName);
         if (!res.valid()) {
             return false;
@@ -20,17 +23,34 @@ namespace trapdoor {
 
         //build in variable
         this->bot.player = p;
+        this->bs.region = &p->getRegion();
+
+        //function binding
         this->engine.new_usertype<BotProxy>("Bot",
                                             "x", &BotProxy::x,
                                             "y", &BotProxy::y,
                                             "z", &BotProxy::z,
                                             "say", &BotProxy::say,
                                             "moveto", &BotProxy::moveto,
-                                            "jump", &BotProxy::jump
+                                            "jump", &BotProxy::jump,
+                                            "runcmd", &BotProxy::runCommand,
+                                            "destroyPosition", &BotProxy::destroyPosition
+        );
+
+        this->engine.new_usertype<LevelProxy>("Level",
+                                              "getTick", &LevelProxy::getTick,
+                                              "getTime", &LevelProxy::getTime,
+                                              "getMeanMspt", &LevelProxy::getMSPT
 
         );
 
+
         this->engine.set("Bot", std::ref(this->bot));
+
+        this->engine.set("Level", std::ref(this->level));
+
+        this->engine.set("Region", std::ref(this->bs));
+
         this->running = true;
         return true;
     }
@@ -50,9 +70,66 @@ namespace trapdoor {
         }
     }
 
+
     void ScriptDriver::stop() {
         //删除所有东西
         this->engine = sol::state(nullptr);
         this->running = false;
     }
+
+
+    //API -- Level
+    int LevelProxy::getTime() { return Global<Level>->getTime(); }
+
+    uint64_t LevelProxy::getTick() { return Global<Level>->getCurrentServerTick().t; }
+
+    double LevelProxy::getMSPT() { return trapdoor::getMeanMSPT(); }
+
+
+    //API -- Bot
+    void BotProxy::say(const string &msg) const {
+        trapdoor::broadcastMessage(fmt::format(" <{}> {}", this->player->getRealName(), msg));
+    }
+
+
+    void BotProxy::jump() const {
+        player->simulateJump();
+    }
+
+    void BotProxy::moveto(float px, float py, float pz) const {
+        player->simulateMoveToLocation({px, py, pz}, 1.0f);
+    }
+
+    void BotProxy::update() {
+        auto pos = player->getPos();
+        x = pos.x;
+        y = pos.y;
+        z = pos.z;
+    }
+
+    void BotProxy::destroyPosition(int px, int py, int pz) const {
+        player->simulateDestroyBlock({px, py, pz}, DF);
+    }
+
+    void BotProxy::interact() const {
+        player->simulateInteract();
+    }
+
+    void BotProxy::attack() const {
+        player->simulateAttack();
+    }
+
+    bool BotProxy::runCommand(const string &cmd) const {
+        player->runcmd(cmd);
+    }
+
+    std::string BlockSourceProxy::getBlockName(int x, int y, int z) {
+        return this->region->getBlock(x, y, z).getTypeName();
+    }
+
+    int BlockSourceProxy::getBlockID(int x, int y, int z) {
+        return this->region->getBlock(x, y, z).getId();
+    }
+
+
 }
--- src/include/ScriptDriver.h
+++ src/include/ScriptDriver.h
@@ -6,41 +6,63 @@
 #define TRAPDOOR_SCRIPTDRIVER_H
 
 #include "sol.hpp"
+#include "MCTick.h"
 #include <mc/SimulatedPlayer.hpp>
 #include "Msg.h"
 
 namespace trapdoor {
+    constexpr auto DF = static_cast<ScriptModuleMinecraft::ScriptFacing>(1);
 
+    //BOT API
+    struct BotProxy {
+        void say(const std::string &msg) const;
+
+        inline void jump() const;
+
+        inline void moveto(float px, float py, float pz) const;
+
+        inline void update();
+
+
+        inline void destroyPosition(int px, int py, int pz) const;
+
+        inline void interact() const;
+
+        inline void attack() const;
+
+        [[nodiscard]] bool runCommand(const std::string &cmd) const;
+
+        SimulatedPlayer *player{nullptr};
+        float x;
+        float y;
+        float z;
+    };
+
+
+    struct BlockSourceProxy {
+        inline void setRegion(BlockSource *bs) { this->region = bs; }
+
+        std::string getBlockName(int x, int y, int z);
+
+        int getBlockID(int x, int y, int z);
+
+        BlockSource *region;
+
+    };
+
+    struct LevelProxy {
+        int getTime();
+
+        uint64_t getTick();
+
+        double getMSPT();
+
+
+    };
+
+    //driver
     class ScriptDriver {
-
     public:
-        //BOT API
-        struct BotProxy {
-            void say(const std::string &msg) const {
-                trapdoor::broadcastMessage(fmt::format(" <{}> {}", this->player->getRealName(), msg));
-            }
-
-            void jump() const {
-                player->simulateJump();
-            }
-
-            void moveto(float px, float py, float pz) const {
-                player->simulateMoveToLocation({px, py, pz}, 1.0f);
-            }
-
-            void update() {
-                auto pos = player->getPos();
-                x = pos.x;
-                y = pos.y;
-                z = pos.z;
-            }
-
-            SimulatedPlayer *player{nullptr};
-            float x;
-            float y;
-            float z;
-        };
-
 
         bool init(const string &fileName, SimulatedPlayer *p, int interval, bool errorStop);
 
@@ -55,6 +77,8 @@ namespace trapdoor {
     private:
         sol::state engine; //engine
         BotProxy bot; //for data binding
+        BlockSourceProxy bs;
+        LevelProxy level;
         bool running{false};
         bool errorStop{true};
         int interval{1};
