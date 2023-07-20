1d4e7b3df173bc0e53958d100b29f47be45a35dd
YuChuXi <81864000+yuxibenxi@users.noreply.github.com>
2023-07-20 20:30:06 +0800

    Update run_bds.ps1 to 1.20.11.01

--- run_bds.ps1
+++ run_bds.ps1
@@ -1,2 +1,2 @@
 $Env:TR_ROOT_PATH = 'C:\Users\xhy\dev\trapdoor-ll\src\root\'
-& "D:\Games\MC\BDS\bedrock-server-1.20.0.01\bedrock_server_mod.exe"
+& "D:\Games\MC\BDS\bedrock-server-1.20.11.01\bedrock_server_mod.exe"
