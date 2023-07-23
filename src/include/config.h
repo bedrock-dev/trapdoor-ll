18ae2e01408f1032bc4c276e154528d8b2bbfcd8
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2023-07-22 21:43:29 -0700

    Release 0.40.0

--- src/include/config.h
+++ src/include/config.h
@@ -2,8 +2,8 @@
 #define STR(x) #x
 #define STRING(x) STR(x)
 #define Trapdoor_VERSION_MAJOR 0
-#define Trapdoor_VERSION_MINOR 39
+#define Trapdoor_VERSION_MINOR 40
 #define Trapdoor_VERSION_PATCH 0
-#define TRAPDOOR_VERSION 0.39.0
-#define GAME_VERSION 1.20.0.01
-#define BUILD_TIME 2023-07-10 12:08:24
+#define TRAPDOOR_VERSION 0.40.0
+#define GAME_VERSION 1.20.11.01
+#define BUILD_TIME 2023-07-23 12:30:00
